5ec24683061fb55a8351e7ddd5a4c6e4ef121c17
Antonio Feregrino <antonio.feregrino@gmail.com>
2024-12-29 08:50:43 +0000

    Add CLI

--- src/main/kotlin/feregri/no/kotdown/KotdownApplication.kt
+++ src/main/kotlin/feregri/no/kotdown/KotdownApplication.kt
@@ -1,11 +1,21 @@
 package feregri.no.kotdown
 
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KotdownApplication
+class KotdownApplication : CommandLineRunner {
+
+    private val log = LoggerFactory.getLogger(KotdownApplication::class.java)
+
+    override fun run(vararg args: String?) {
+        log.info(args.joinToString())
+        println("Hello World")
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<KotdownApplication>(*args)
+    runApplication<KotdownApplication>(*args)
 }
